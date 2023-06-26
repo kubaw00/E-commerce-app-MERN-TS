b32bc4d34baf5386de9b690da622684ef6f16867
Jakub Widurek <jakubwidurek@gmail.com>
2023-06-26 14:44:41 +0200

    update paginate and product carousel

--- frontend/src/components/Paginate.tsx
+++ frontend/src/components/Paginate.tsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
-  return (
-    pages > 1 && (
-      <Pagination>
-        {[...Array(pages).keys()].map((x) => (
-          <Pagination.Item
-            as={Link}
-            key={x + 1}
-            href={
-              !isAdmin
-                ? keyword
-                  ? `/search/${keyword}/page/${x + 1}`
-                  : `/page/${x + 1}`
-                : `/admin/productlist/${x + 1}`
-            }
-            active={x + 1 === page}
-          >
-            {x + 1}
-          </Pagination.Item>
-        ))}
-      </Pagination>
-    )
+interface PaginateProps {
+  page: number;
+  pages: number;
+  isAdmin: boolean;
+  keyword: string;
+}
+
+export const Paginate: React.FC<PaginateProps> = ({
+  pages,
+  page,
+  isAdmin,
+  keyword,
+}) => {
+  return pages > 1 ? (
+    <Pagination>
+      {[...Array(pages).keys()].map((x) => (
+        <LinkContainer
+          key={x + 1}
+          to={
+            !isAdmin
+              ? keyword
+                ? `/search/${keyword}/page/${x + 1}`
+                : `/page/${x + 1}`
+              : `/admin/productlist/${x + 1}`
+          }
+        >
+          <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+        </LinkContainer>
+      ))}
+    </Pagination>
+  ) : (
+    <span></span>
   );
 };
-
-export default Paginate;
--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -1,34 +1,34 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Image, Carousel } from 'react-bootstrap';
-import Loader from './Loader';
-import Message from './Message';
-import { listTopProducts } from '../actions/productActions';
+import { Carousel, Image } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { listTopProducts } from '../actions/productActions';
+import { RootStore } from '../store';
+import { Loader } from './Loader';
+import { Message } from './Message';
 
-const ProductCarousel = () => {
+export const ProductCarousel: React.FC = () => {
   const dispatch = useDispatch();
-
-  const productTopRated = useSelector((state) => state.productTopRated);
-  const { loading, error, products } = productTopRated;
+  const { loading, error, products } = useSelector(
+    (state: RootStore) => state.productTop
+  );
 
   useEffect(() => {
     dispatch(listTopProducts());
   }, [dispatch]);
-
   return loading ? (
     <Loader />
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
     <Carousel pause='hover' className='bg-dark'>
-      {products.map((product) => (
+      {products?.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
+          <Link to={`/products/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2>
-                {product.name} (${product.price})
+                {product.name} ({product.price})
               </h2>
             </Carousel.Caption>
           </Link>
@@ -37,5 +37,3 @@ const ProductCarousel = () => {
     </Carousel>
   );
 };
-
-export default ProductCarousel;
